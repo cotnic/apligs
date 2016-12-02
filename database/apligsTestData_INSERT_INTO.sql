820d06f0cb91472b9429313731475352d35b8784
mj0073 <mj0073@student.uni-lj.si>
2016-12-02 13:22:22 +0100

    Created database tables and inserted test data

---
+++ database/apligsTestData_INSERT_INTO.sql
@@ -0,0 +1,90 @@
+insert into lokacija (postna_stevilka, lo_naziv) values (2380, 'Slovenj Gradec');
+
+insert into lokacija (postna_stevilka, lo_naziv) values (1000, 'Ljubljana');
+
+insert into lokacija (postna_stevilka, lo_naziv) values (2000, 'Maribor');
+
+insert into lokacija (postna_stevilka, lo_naziv) values (3301, 'Petrovce');
+
+insert into lokacija (postna_stevilka, lo_naziv) values (1290 , 'Grosuplje');
+
+insert into zvrst (id_zvrst, zv_naziv) values (2, 'Rock');
+
+insert into zvrst (id_zvrst, zv_naziv) values (1, 'rap');
+
+insert into zvrst (id_zvrst, zv_naziv) values (4, 'country');
+
+insert into zvrst (id_zvrst, zv_naziv) values (0, 'reggae');
+
+insert into zvrst (id_zvrst, zv_naziv) values (3, 'salsa');
+
+insert into instrument (id_instrument, in_naziv) values (1, 'kitara');
+
+insert into instrument (id_instrument, in_naziv) values (4, 'harmonika');
+
+insert into instrument (id_instrument, in_naziv) values (3, 'bobni');
+
+insert into instrument (id_instrument, in_naziv) values (2, 'bas kitara');
+
+insert into instrument (id_instrument, in_naziv) values (0, 'klavir');
+
+insert into vloga (id_vloga, vl_naziv) values (0, 'pevec');
+
+insert into vloga (id_vloga, vl_naziv) values (1, 'kitarist');
+
+insert into vloga (id_vloga, vl_naziv) values (2, 'basist');
+
+insert into vloga (id_vloga, vl_naziv) values (4, 'pianist');
+
+insert into vloga (id_vloga, vl_naziv) values (3, 'back vocal');
+
+insert into uporabnik (id_uporabnik, postna_stevilka, id_vloga, up_ime, up_priimek, up_email, up_ustvarjen) values (2, 2380, 3, 'Mitja', 'Kotnik', 'mj0073@student.uni-lj.si', '2016-2-25');
+
+insert into uporabnik (id_uporabnik, postna_stevilka, id_vloga, up_ime, up_priimek, up_email, up_ustvarjen) values (1, 2000, 0, 'Rok', 'Solar', 'rs0424@student.uni-lj.si', '2016-11-22');
+
+insert into uporabnik (id_uporabnik, postna_stevilka, id_vloga, up_ime, up_priimek, up_email, up_ustvarjen) values (4, 1000, 1, 'Luka', 'Lavtar', 'll0523@student.uni-lj.si', '2016-11-13');
+
+insert into uporabnik (id_uporabnik, postna_stevilka, id_vloga, up_ime, up_priimek, up_email, up_ustvarjen) values (3, 3301, 0, 'Andrej', 'Kokelj', 'ak1729@student.uni-lj.si', CURDATE());
+
+insert into uporabnik (id_uporabnik, postna_stevilka, id_vloga, up_ime, up_priimek, up_email, up_ustvarjen) values (0, 1290, 4, 'Miha', 'Lampret', 'ml0043@student.uni-lj.si', '2016-11-19');
+
+insert into igra (id_instrument, id_uporabnik) values (1, 2);
+
+insert into igra (id_instrument, id_uporabnik) values (4, 1);
+
+insert into igra (id_instrument, id_uporabnik) values (3, 4);
+
+insert into igra (id_instrument, id_uporabnik) values (2, 3);
+
+insert into igra (id_instrument, id_uporabnik) values (0, 0);
+
+insert into izvaja (id_zvrst, id_uporabnik) values (2, 2);
+
+insert into izvaja (id_zvrst, id_uporabnik) values (1, 1);
+
+insert into izvaja (id_zvrst, id_uporabnik) values (4, 4);
+
+insert into izvaja (id_zvrst, id_uporabnik) values (0, 3);
+
+insert into izvaja (id_zvrst, id_uporabnik) values (3, 0);
+
+insert into sporocilo (id_sporocila, id_poslje, id_prejme, sp_zadeva, sp_vsebina, sp_poslano) values (1, 2, 1, 'Pozdrav', 'Test aplikacije', '2016-12-13');
+
+insert into sporocilo (id_sporocila, id_poslje, id_prejme, sp_zadeva, sp_vsebina, sp_poslano) values (2, 1, 2, 'Zivjo', 'Kako si kaj danes?', '2016-12-13');
+
+insert into sporocilo (id_sporocila, id_poslje, id_prejme, sp_zadeva, sp_vsebina, sp_poslano) values (4, 2, 4, 'Kitarist', 'Pozdravljen, opazili smo, da si kitarist. Bi se nam pridruzil?', '2016-12-13');
+
+insert into sporocilo (id_sporocila, id_poslje, id_prejme, sp_zadeva, sp_vsebina, sp_poslano) values (3, 1, 2, 'Ansambel', 'Oj, sem opazil, da iscete pevca.', '2016-12-13');
+
+insert into sporocilo (id_sporocila, id_poslje, id_prejme, sp_zadeva, sp_vsebina, sp_poslano) values (0, 3, 1, 'Kritika', 'Niste glih najboljsi', '2016-12-13');
+
+insert into oglas (id_oglas, id_vloga, id_uporabnik, id_zvrst, og_naslov, og_objavljen, og_opis, og_premium) values (4, 0, 4, 0, 'Ansambel', '2016-12-1', 'Iscemo pevca za v ansambel!', 1);
+
+insert into oglas (id_oglas, id_vloga, id_uporabnik, id_zvrst, og_naslov, og_objavljen, og_opis, og_premium) values (0, 2, 1, 2, 'Pevec', '2016-12-1', 'Iscemo pevca za petje v duetu!', 0);
+
+insert into oglas (id_oglas, id_vloga, id_uporabnik, id_zvrst, og_naslov, og_objavljen, og_opis, og_premium) values (2, 2, 4, 1, 'Kitarist', '2016-12-1', 'Potrebujemo se enega kitarista, ki bi zamenjal obstojecega.', 1);
+
+insert into oglas (id_oglas, id_vloga, id_uporabnik, id_zvrst, og_naslov, og_objavljen, og_opis, og_premium) values (3, 0, 2, 0, 'Back vocal', '2016-12-1', 'Iscemo back vocal za pop skupino', 0);
+
+insert into oglas (id_oglas, id_vloga, id_uporabnik, id_zvrst, og_naslov, og_objavljen, og_opis, og_premium) values (1, 2, 3, 0, 'Rock-punk', '2016-12-1', 'Iscemo pianista za jazz skupino', 0);
+
---
+++ database/apligs_CREATE_DATABASE.sql
@@ -0,0 +1,162 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2. 12. 2016 12:38:36                         */
+/*==============================================================*/
+
+
+drop table if exists igra;
+
+drop table if exists instrument;
+
+drop table if exists izvaja;
+
+drop table if exists lokacija;
+
+drop table if exists oglas;
+
+drop table if exists sporocilo;
+
+drop table if exists uporabnik;
+
+drop table if exists vloga;
+
+drop table if exists zvrst;
+
+/*==============================================================*/
+/* Table: igra                                                  */
+/*==============================================================*/
+create table igra
+(
+   id_instrument        int not null,
+   id_uporabnik         int not null,
+   primary key (id_instrument, id_uporabnik)
+);
+
+/*==============================================================*/
+/* Table: instrument                                            */
+/*==============================================================*/
+create table instrument
+(
+   id_instrument        int not null,
+   in_naziv             varchar(50) not null,
+   primary key (id_instrument)
+);
+
+/*==============================================================*/
+/* Table: izvaja                                                */
+/*==============================================================*/
+create table izvaja
+(
+   id_zvrst             int not null,
+   id_uporabnik         int not null,
+   primary key (id_zvrst, id_uporabnik)
+);
+
+/*==============================================================*/
+/* Table: lokacija                                              */
+/*==============================================================*/
+create table lokacija
+(
+   postna_stevilka      int not null,
+   lo_naziv             varchar(60) not null,
+   primary key (postna_stevilka)
+);
+
+/*==============================================================*/
+/* Table: oglas                                                 */
+/*==============================================================*/
+create table oglas
+(
+   id_oglas             int not null,
+   id_vloga             int not null,
+   id_uporabnik         int not null,
+   id_zvrst             int not null,
+   og_naslov            varchar(30) not null,
+   og_objavljen         date not null,
+   og_opis              varchar(200) not null,
+   og_premium           bool not null,
+   primary key (id_oglas)
+);
+
+/*==============================================================*/
+/* Table: sporocilo                                             */
+/*==============================================================*/
+create table sporocilo
+(
+   id_sporocila         int not null,
+   id_poslje            int not null,
+   id_prejme            int not null,
+   sp_zadeva            varchar(25) not null,
+   sp_vsebina           varchar(250) not null,
+   sp_poslano         	date not null,
+   primary key (id_sporocila)
+);
+
+/*==============================================================*/
+/* Table: uporabnik                                             */
+/*==============================================================*/
+create table uporabnik
+(
+   id_uporabnik         int not null,
+   postna_stevilka      int not null,
+   id_vloga             int not null,
+   up_ime               varchar(30) not null,
+   up_priimek           varchar(50) not null,
+   up_email             varchar(70) not null,
+   up_ustvarjen         date,
+   primary key (id_uporabnik)
+);
+
+/*==============================================================*/
+/* Table: vloga                                                 */
+/*==============================================================*/
+create table vloga
+(
+   id_vloga             int not null,
+   vl_naziv             varchar(25) not null,
+   primary key (id_vloga)
+);
+
+/*==============================================================*/
+/* Table: zvrst                                                 */
+/*==============================================================*/
+create table zvrst
+(
+   id_zvrst             int not null,
+   zv_naziv             varchar(50) not null,
+   primary key (id_zvrst)
+);
+
+alter table igra add constraint FK_igra foreign key (id_instrument)
+      references instrument (id_instrument) on delete restrict on update restrict;
+
+alter table igra add constraint FK_igra2 foreign key (id_uporabnik)
+      references uporabnik (id_uporabnik) on delete restrict on update restrict;
+
+alter table izvaja add constraint FK_izvaja foreign key (id_zvrst)
+      references zvrst (id_zvrst) on delete restrict on update restrict;
+
+alter table izvaja add constraint FK_izvaja2 foreign key (id_uporabnik)
+      references uporabnik (id_uporabnik) on delete restrict on update restrict;
+
+alter table oglas add constraint FK_ima1 foreign key (id_zvrst)
+      references zvrst (id_zvrst) on delete restrict on update restrict;
+
+alter table oglas add constraint FK_objavi foreign key (id_uporabnik)
+      references uporabnik (id_uporabnik) on delete restrict on update restrict;
+
+alter table oglas add constraint FK_vsebovan foreign key (id_vloga)
+      references vloga (id_vloga) on delete restrict on update restrict;
+
+alter table sporocilo add constraint FK_poslje foreign key (id_prejme)
+      references uporabnik (id_uporabnik) on delete restrict on update restrict;
+
+alter table sporocilo add constraint FK_prejme foreign key (id_poslje)
+      references uporabnik (id_uporabnik) on delete restrict on update restrict;
+
+alter table uporabnik add constraint FK_biva foreign key (postna_stevilka)
+      references lokacija (postna_stevilka) on delete restrict on update restrict;
+
+alter table uporabnik add constraint FK_ima foreign key (id_vloga)
+      references vloga (id_vloga) on delete restrict on update restrict;
+
