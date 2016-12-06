729078958747520a2815e66d2631bed784167ffa
mj0073 <mj0073@student.uni-lj.si>
2016-12-06 14:52:25 +0100

    Working with json and gets data from MySQL table

--- REST/src/apligs/rest/status/V1_status.java
+++ REST/src/apligs/rest/status/V1_status.java
@@ -1,7 +1,11 @@
 package apligs.rest.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+
+import java.sql.*;
+
+import apligs.dao.*;
 
 @Path(("/v1/status"))
 public class V1_status {
@@ -12,11 +16,43 @@ public class V1_status {
 	public String returnTitle() {
 		return "<p>Java Web Service</p>";
 	}
-	
+
 	@Path("/version")
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnVersion() {
-		return apiVersion;
+		return "<p>Version: </p>" + apiVersion;
+	}
+
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception {
+
+		PreparedStatement query = null;
+		String myString = null;
+		String returnString = null;
+		Connection conn = null;
+
+		try {
+			conn = MySQLApligs.MySQLApligs().getConnection();
+			query = conn.prepareStatement("select DATE_FORMAT(NOW(),'%d.%m.%Y %H:%i:%s') as date");
+			ResultSet rs = query.executeQuery();
+
+			while (rs.next()) {
+				myString = rs.getString("date");
+			}
+
+			query.close();
+
+			returnString = "<p>Database Status</p> " + "<p>Database Date/Time return: " + myString + "</p>";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(conn != null) conn.close();
+		}
+
+		return returnString;
 	}
 }
